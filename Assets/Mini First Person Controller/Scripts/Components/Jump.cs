86fad402d437e952e53a81a24f87272513bbefe7
Mhykoi <kaanuyann@gmail.com>
2022-06-07 20:22:27 +0300

    Commit 2

--- Assets/Mini First Person Controller/Scripts/Components/Jump.cs
+++ Assets/Mini First Person Controller/Scripts/Components/Jump.cs
@@ -3,8 +3,9 @@
 public class Jump : MonoBehaviour
 {
     Rigidbody rigidbody;
-    public float jumpStrength = 2;
+    public float jumpStrength = 4;
     public event System.Action Jumped;
+    public bool canJump = true;
 
     [SerializeField, Tooltip("Prevents jumping when the transform is in mid-air.")]
     GroundCheck groundCheck;
@@ -25,10 +26,14 @@ public class Jump : MonoBehaviour
     void LateUpdate()
     {
         // Jump when the Jump button is pressed and we are on the ground.
-        if (Input.GetButtonDown("Jump") && (!groundCheck || groundCheck.isGrounded))
+        if (canJump)
         {
-            rigidbody.AddForce(Vector3.up * 100 * jumpStrength);
-            Jumped?.Invoke();
+            if (Input.GetButtonDown("Jump") && (!groundCheck || groundCheck.isGrounded) && Time.timeScale != 0)
+            {
+                rigidbody.AddForce(Vector3.up * 100 * jumpStrength);
+                Jumped?.Invoke();
+            }
         }
+
     }
 }

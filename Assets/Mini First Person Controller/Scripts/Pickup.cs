608800192c8266b15ecd63e79d04410dc1fc71ec
Mhykoi <kaanuyann@gmail.com>
2022-06-14 11:30:25 +0300

    Commit 3

--- Assets/Mini First Person Controller/Scripts/Pickup.cs
+++ Assets/Mini First Person Controller/Scripts/Pickup.cs
@@ -54,7 +54,12 @@ public class Pickup : MonoBehaviour
             if (pickObj.GetComponent<Rigidbody>()&& pickObj.tag==("holdable"))
             {
                 Rigidbody objRig = pickObj.GetComponent<Rigidbody>();
-            pickObj.GetComponent<Rigidbody>().isKinematic = true;
+            //pickObj.GetComponent<Rigidbody>().isKinematic = true;
+            pickObj.GetComponent<Rigidbody>().freezeRotation = true;
+            pickObj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            pickObj.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            pickObj.GetComponent<Rigidbody>().mass = 20;
+            pickObj.GetComponent<Rigidbody>().angularDrag = 100;
             pickObj.GetComponent<Rigidbody>().useGravity = false;
             objRig.useGravity = false;
                 objRig.drag = 10;
@@ -68,13 +73,17 @@ public class Pickup : MonoBehaviour
     void DropObject()
     {
         Rigidbody heldRig = heldObj.GetComponent<Rigidbody>();
-        heldObj.GetComponent<Rigidbody>().isKinematic = false;
+        //heldObj.GetComponent<Rigidbody>().isKinematic = false;
+        heldObj.GetComponent<Rigidbody>().freezeRotation = false;
+        heldObj.GetComponent<Rigidbody>().mass = 2;
         heldObj.GetComponent<Rigidbody>().useGravity = true;
+        heldObj.GetComponent<Rigidbody>().angularDrag = 20;
+        heldObj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        heldObj.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
         heldRig.useGravity = true;
         heldRig.drag = 1;
 
         heldRig.transform.parent = null;
         heldObj = null;
     }
-
 }
